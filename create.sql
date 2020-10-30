01e2acc3f1664ddd895a67c6569aa833e633d800
Joao Sousa <joaosousa.pt@gmail.com>
2020-10-30 19:10:35 +0000

    Updated SQLite file. 1st Draft Finished (?)

--- create.sql
+++ create.sql
@@ -3,31 +3,51 @@
 
 PRAGMA foreign_keys=OFF;
 
-
 DROP TABLE  IF EXISTS UserEntity;
+DROP TABLE  IF EXISTS PetPosting;
+DROP TABLE  IF EXISTS Photo;
+DROP TABLE  IF EXISTS AdoptionProposal;
+DROP TABLE  IF EXISTS PetQuestions;
 
 PRAGMA foreign_keys=ON;
 
 CREATE TABLE UserEntity (
     username        VARCHAR(255) PRIMARY KEY,
-    password        VARCHAR(255)            ,                     
+    password        VARCHAR(255) NOT NULL   ,
+    photo           INT          NOT NULL REFERENCES Photo(id) ON UPDATE CASCADE,    
 );
 
 -- petSize type is subject to change. atm is the length of the animal
-CREATE TABLE Pet (
-    name            VARCHAR(255) PRIMARY KEY,
-    species         VARCHAR(255)            ,
+-- location as a varchar(255) or a table itself ?
+CREATE TABLE PetPosting (
+    user            VARCHAR(255) NOT NULL REFERENCES UserEntity(name) ON UPDATE CASCADE,
+    petName         VARCHAR(255) NOT NULL   ,
+    species         VARCHAR(255) NOT NULL   ,
     petSize         INT          NOT NULL   ,
-    color           VARCHAR(255)            ,
+    color           VARCHAR(255) NOT NULL   ,
     photo           INT          NOT NULL REFERENCES Photo(id) ON UPDATE CASCADE,
-    location        INT         NOT NULL REFERENCES Location(id) ON UPDATE CASCADE, 
+    location        VARCHAR(255) NOT NULL   ,
+    PRIMARY KEY (user, petName),
 );
 
+-- temporary. how to store image in sqlite? or is it even needed?
 CREATE TABLE Photo (
     id              INT          PRIMARY KEY,
-    -- link ?
+    image           BLOB                    ,
 );
 
-CREATE TABLE Location (
+-- status either is 'P' (pending), 'A' (accepted) or 'R' (refused)
+CREATE TABLE AdoptionProposal (
     id              INT          PRIMARY KEY,
+    user            VARCHAR(255) NOT NULL REFERENCES UserEntity(name) ON UPDATE CASCADE,
+    pet             VARCHAR(255) NOT NULL REFERENCES PetPosting(name) ON UPDATE CASCADE,
+    status          char(1)      NOT NULL DEFAULT 'P' CHECK (status in ('P', 'A', 'R'))  ,
+);
+
+-- necessary?
+CREATE TABLE PetQuestions (
+    id              INT          PRIMARY KEY,
+    user            VARCHAR(255) NOT NULL REFERENCES UserEntity(name) ON UPDATE CASCADE,
+    pet             VARCHAR(255) NOT NULL REFERENCES PetPosting(name) ON UPDATE CASCADE,
+    question        VARCHAR(255) NOT NULL,
 );
