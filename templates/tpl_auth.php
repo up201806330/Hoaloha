9316c5b7f20baf0d8086192c53cd75d3afcedc02
Diogo Almeida <diogohenriquealmeida@hotmail.com>
2020-11-18 15:38:25 +0000

    Added CSS to the register page

--- templates/tpl_auth.php
+++ templates/tpl_auth.php
@@ -8,6 +8,7 @@
       <a href="#" id="closeButton" class="close"><span class="material-icons-round">close</span></a>
     </div>
     <h1>Log In</h1>
+    <h2>Welcome Back!</h2>
     <form method="post" action="../actions/action_login.php">
       <div class="txt_field">
         <input type="text" name="username" required>
@@ -41,18 +42,37 @@
  * Draws the register section.
  */ ?>
   <section id="signup">
-
-    <header><h2>New Account</h2></header>
-
-    <form method="post" action="../actions/action_register.php">
-      <input type="text" name="username" placeholder="username" required>
-      <input type="password" name="password" placeholder="password" required>
-      <input type="file" name="profile_img" accept="image/*">
-      <input type="number" name="phone_number" placeholder="9** *** ***" required>
-      <input type="email" name="email" placeholder="example@here.com" required>
-      <input type="text" name="location" placeholder="TEMPORARY" required>
-      <input type="submit" value="Signup">
-    </form>
-
+    <div class="register-content">
+      <h1>Sign Up</h1>
+      <h2>New Here? Welcome!</h2>
+      <form method="post" action="../actions/action_register.php">
+        <div class="txt_field">
+          <input type="text" name="username" required>
+          <span></span>
+          <label>Username</label>
+        </div>
+        <div class="txt_field">
+          <input type="password" name="password" required>
+          <span></span>
+          <label>Password</label>
+        </div>
+        <div class="txt_field">
+          <input type="number" name="phone_number" required>
+          <span></span>
+          <label>Phone Number</label>
+        </div>
+        <div class="txt_field">
+          <input type="email" name="email" required>
+          <span></span>
+          <label>Email</label>
+        </div>
+        <div class="txt_field">
+          <input type="text" name="location" required>
+          <span></span>
+          <label>Location</label>
+        </div>
+        <button type="submit">SIGN UP</button>
+      </form>
+    </div>
   </section>
 <?php } ?>
