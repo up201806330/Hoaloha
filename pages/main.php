a645de6c3e8ec55e252679c9bb2d98fc79b95d0e
Pedro Queirós <pedromsqueiros@gmail.com>
2020-11-21 16:01:55 +0000

    Now its possible to store images on the database

--- pages/main.php
+++ pages/main.php
@@ -1,4 +1,5 @@
 <?php 
+  include_once('../database/db_image.php');
   include_once('../templates/tpl_common.php');
   include_once('../templates/tpl_navbar.php'); 
   include_once('../templates/tpl_mainpage.php'); 
@@ -7,4 +8,5 @@
   draw_navbar();
   draw_mainpage_body();
   draw_footer();
+
 ?>
