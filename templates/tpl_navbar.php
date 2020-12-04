d29f9e0467a5235540e93996e6a2d66923dd7096
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-12-04 19:28:58 +0000

    About section

--- templates/tpl_navbar.php
+++ templates/tpl_navbar.php
@@ -2,12 +2,13 @@
     /**
  * Draws the navbar for all pages.
  */?>
-    <div class="topnav">
-        <ul class = "topnav-elements">
-            <li><a class="active" href="#home">Home</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#contact">Contact Us</a></li>
-        </ul>
+
+    <button id="collapsible" type="button" class="collapsible">About Us</button>
+    <div class="content">
+        <p><a class="name">Diogo Almeida</a>    <a class="up">up201806630</a></p>
+        <p><a class="name">João Sousa</a>       <a class="up">up201806613</a></p>
+        <p><a class="name">Pedro Queirós</a>    <a class="up">up201806329</a></p>
+        <p><a class="name">Rafael Ribeiro</a>   <a class="up">up201806330</a></p>
     </div>
 <?php } ?>
 
