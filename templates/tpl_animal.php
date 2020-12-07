ea9c0cd65d5295c106732cb428b8f62a8fc8044d
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-12-07 16:54:02 +0000

    Search done

--- templates/tpl_animal.php
+++ templates/tpl_animal.php
@@ -78,7 +78,7 @@
           <span></span>
           <label>Description</label>
         </div>
-        <select name='species' style='height: 45px; font-family:Arial, FontAwesome;'>
+        <select name='species'>
             <option value=''>&#xf039; &nbsp; Select a Species</option>
             <option value='Dog'>&#xf6d3; &nbsp; Dog</option>
             <option value='Cat'>&#xf6be; &nbsp; Cat</option>
@@ -91,10 +91,10 @@
           <span></span>
           <label>Breed</label>
         </div>
-        <select name='gender' style='height: 45px; font-family:Arial, FontAwesome;'>
+        <select name='gender'>
             <option value=''>&#xf22d; &nbsp; Select a Gender</option>
-            <option value='Male'>&#xf222; &nbsp; Male</option>
-            <option value='Female'>&#xf221; &nbsp; Female</option>
+            <option value='male'>&#xf222; &nbsp; Male</option>
+            <option value='fem'>&#xf221; &nbsp; Female</option>
             <label>Gender</label>
         </select>
         <div class="details">
@@ -108,7 +108,7 @@
             <span></span>
             <label>Color</label>
             </div>
-            <select name='dimensions' style='height: 45px; font-family:Arial, FontAwesome;'>
+            <select name='dimensions'>
                 <option value=''>&#xf22d; &nbsp; Select Dimensions</option>
                 <option value='Small'>&#xf56b; &nbsp; Small</option>
                 <option value='Medium'>&#xf546; &nbsp; Medium</option>
--- templates/tpl_mainpage.php
+++ templates/tpl_mainpage.php
@@ -1,14 +1,15 @@
 <?php 
 function draw_mainpage_body() {
 ?>
-        <div class="image-container">
-            <h1>Find the Right Animal for You</h1>
-            <h2>Check out our list of Available Pets</h2>
-            <div class="button-container">
-                <div class="inner-button-container">
-                    <button id="searchButton" class="btn btn1">Search a Pet    <span class="material-icons-round">search</span></button>
-                    <button onclick="window.location='../pages/add_animal.php';" class="btn btn2">Rehome a Pet   <span class="material-icons-round">home</span></button>
-                </div>
+
+    <div class="image-container">
+        <h1>Find the Right Animal for You</h1>
+        <h2>Check out our list of Available Pets</h2>
+        <div class="button-container">
+            <div class="inner-button-container">
+                <button id="searchButton" class="btn btn1">Search a Pet    <span class="material-icons-round">search</span></button>
+                <button onclick="window.location='../pages/add_animal.php';" class="btn btn2">Rehome a Pet   <span class="material-icons-round">home</span></button>
             </div>
         </div>
+    </div>
 <?php } ?>
