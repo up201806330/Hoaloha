4744955988a095d462859de0a310a8e268a40860
JoaoASousa <45333685+JoaoASousa@users.noreply.github.com>
2020-12-09 04:01:10 +0000

    Changes divs and paragraphs to ul and li

--- templates/tpl_navbar.php
+++ templates/tpl_navbar.php
@@ -3,13 +3,13 @@
  * Draws the navbar for all pages.
  */?>
     <div class="navbar">
-        <button id="collapsible" type="button" class="collapsible">About Us</button>
-        <div class="content">
-            <p><a class="name">Diogo Almeida</a>    <a class="up">up201806630</a></p>
-            <p><a class="name">João Sousa</a>       <a class="up">up201806613</a></p>
-            <p><a class="name">Pedro Queirós</a>    <a class="up">up201806329</a></p>
-            <p><a class="name">Rafael Ribeiro</a>   <a class="up">up201806330</a></p>
-        </div>
+        <button id="collapsible" class="collapsible">About Us</button>
+        <ul class="content">
+            <li><a class="name">Diogo Almeida</a>    <a class="up">up201806630</a></li>
+            <li><a class="name">João Sousa</a>       <a class="up">up201806613</a></li>
+            <li><a class="name">Pedro Queirós</a>    <a class="up">up201806329</a></li>
+            <li><a class="name">Rafael Ribeiro</a>   <a class="up">up201806330</a></li>
+        </ul>
     </div>
 <?php } ?>
 
