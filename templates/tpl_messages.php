31b01b34f0bc3e1a8893e28874456e805355aec1
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-12-17 22:43:21 +0000

    lul

--- templates/tpl_messages.php
+++ templates/tpl_messages.php
@@ -10,7 +10,7 @@ function draw_messages() {
                 <div class="txt_field">
                     <?php 
                     if (@$_SESSION['messages']){
-                        echo '<div id="' . $_SESSION['messages'])['type'] . '-message">' . array_pop($_SESSION['messages'])['content'] . '</div>';
+                        echo '<div id="' . $_SESSION['messages']['type'] . '-message">' . array_pop($_SESSION['messages'])['content'] . '</div>';
                         // array_pop($_SESSION['messages'])['type'];  error poe texto vermelho ; success poe verde
                         unset($_SESSION['messages']);
                     }
