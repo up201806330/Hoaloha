ab81d35f8ae5715aafc651a9fae07b1041a9d60f
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-12-18 03:59:39 +0000

    Hot fix in search

--- pages/list_animals.php
+++ pages/list_animals.php
@@ -12,10 +12,11 @@
   draw_navbar();
   echo '<div class="animal-list-container">';
 
-  $topics = $_SESSION['search_results'];
-  list($maxWeight, $maxAge) = get_max_weight_and_age($topics);
+  $allTopics = getAllTopics();
+  list($maxWeight, $maxAge) = get_max_weight_and_age($allTopics);
 
   draw_search_parameters($maxWeight, $maxAge);
+  $topics = $_SESSION['search_results'];
   draw_n_results(count($_SESSION['search_results']));
   start_animal_list();
   foreach($topics as &$topic){
