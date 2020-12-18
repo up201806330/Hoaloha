0d7b46e46a6983e29dff52fba94f0b5f5d747eed
up201806330 <45789887+up201806330@users.noreply.github.com>
2020-12-18 00:43:43 +0000

    Fixed conflict

--- templates/tpl_animal.php
+++ templates/tpl_animal.php
@@ -102,9 +102,6 @@
           </div>
         <?php } ?>
 
-        <!-- Next and previous buttons -->
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
       </div>
 
       <br>
